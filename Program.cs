d5ec250c5a4d0893fe07f4b12bdedf4619a33325
Florian <13017799@student.hhs.nl>
2019-12-02 19:36:55 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Quiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 2;
+            var b = 3;
+            var c = a + b;
+            Console.WriteLine(c);
+            Console.ReadKey(true);
+        }
+    }
+}
